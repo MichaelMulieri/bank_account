1d1f4e846817876cdd31d890007d165ff1e74477
MichaelMulieri <mikemulieri@gmail.com>
2022-07-28 20:44:21 -0400

    added multiple accounts for each user

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,11 @@
 
-# class User():
-#     def __init__(self, name, email):
-#         self.name = name
-#         self.email = email
-#         self.account = BankAccount(int_rate=0.02, balance = 0)
 
 class BankAccount():
-    def __init__(self, int_rate):
+
+    def __init__(self, int_rate, balance):
         self.int_rate = int_rate
-        self.balance = 0
+        self.balance = balance
+        
 
     def deposit(self, amount):
         self.balance += amount
@@ -19,7 +16,7 @@ class BankAccount():
         return self
 
     def display_acount_info(self):
-        print(f"Balance: ${round(self.balance)}")
+        return f"Balance: ${round(self.balance)}"
 
     def yield_interest(self):
         if self.balance > 0:
@@ -28,9 +25,30 @@ class BankAccount():
         else:
             print(f"You're balance must be more than 0 to earn interest")
 
+class User():
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = {"checking": BankAccount(int_rate=0.02, balance = 0),
+                        "savings": BankAccount(int_rate=0.02, balance= 0)}
+    
+    def displayUserBalance(self):
+        print(f"User: {self.name} Checking Balance: {self.account['checking'].display_acount_info()}")
+        print(f"User: {self.name} Savings Balance: {self.account['savings'].display_acount_info()}")
+        return self
 
-account_one = BankAccount(0.03)
-account_two = BankAccount(0.01)
 
-account_one.deposit(700).deposit(500).deposit(150).withdrawal(100).yield_interest().display_acount_info()
-account_two.deposit(300).deposit(500).withdrawal(25).withdrawal(50).withdrawal(70).withdrawal(40).yield_interest().display_acount_info()
+
+joe = User('Joe', 'Joemail')
+
+joe.account['checking'].deposit(300)
+joe.account['savings'].deposit(700)
+
+joe.displayUserBalance()
+
+
+
+
+
+
+
